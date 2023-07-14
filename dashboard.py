ece9f1117a5d03aec491220b697ce0de9d81ef71
sousiecharle <139395496+sousiecharle@users.noreply.github.com>
2023-07-14 16:13:59 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,24 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import pandas as pd
+
+app = FastAPI()
+
+# Charger les données et les fusionner
+clics = pd.read_csv("clics.csv")
+impressions = pd.read_csv("impressions.csv")
+achats = pd.read_csv("achats.csv")
+
+merged_data = pd.merge(clics, impressions, on="cookie_id")
+merged_data = pd.merge(merged_data, achats, on="cookie_id")
+
+
+@app.get("/charlesapi/data")
+async def get_data():
+    return JSONResponse(content=merged_data.to_dict(orient="records")) # Retourne les données au format JSON
+if _name_ == "_main_":
+    uvicorn.run(app,host="127.0.0.1",port=8000)
+
 import streamlit as st
 import requests
 import pandas as pd
